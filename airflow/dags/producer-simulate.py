3100f9fe5b670042acbe5a3feb0e1ef646d89121
Priya Krishnan <priya_krisshnan@yahoo.co.uk>
2025-08-28 21:30:32 +0530

    Update producer-simulate.py
    made changes to get the latest id in "trainsearchstream" and increment by 1 to keep track of newly inserted records.

--- airflow/dags/producer-simulate.py
+++ airflow/dags/producer-simulate.py
@@ -10,29 +10,38 @@ def insert_records():
     conn = hook.get_conn()
     cur = conn.cursor()
 
-    for _ in range(100):  # insert ~100 rows
+    #Get the last id and increment by 1 to keep track of the generated rows
+    cur.execute("SELECT COALESCE(MAX(id), 0) FROM trainsearchstream")
+    last_id = cur.fetchone()[0]
+
+    new_rows = []
+    for i in range(1,10):  # insert ~10 rows
+        next_id = last_id + i
         obj_type = random.choice([1, 2, 3])
         is_click = None
         if obj_type in [1, 2]:
             is_click = 1 if random.random() < 0.05 else 0
-        row = (
-            random.randint(1, 1e9),   # id
+        
+        new_rows.append((
+            next_id,   # id
             random.randint(1, 1e6),   # searchid
             random.randint(1, 1e6),   # adid
             random.randint(1, 10),    # position
             obj_type,
             round(random.random(), 6),# histctr
             is_click
-        )
-        cur.execute(
-            """INSERT INTO trainsearchstream
-               (id, searchid, adid, position, objecttype, histctr, isclick)
-               VALUES (%s,%s,%s,%s,%s,%s,%s)""", row
-        )
+        ))
+        for row in new_rows:
+            cur.execute(
+                """INSERT INTO trainsearchstream
+                (id, searchid, adid, position, objecttype, histctr, isclick)
+                VALUES (%s,%s,%s,%s,%s,%s,%s)""", row
+            )
 
     conn.commit()
     cur.close()
     conn.close()
+    print(f"Inserted {len(new_rows)} new records into trainsearchstream.")
 
 default_args = {
     "owner": "airflow",
@@ -49,6 +58,6 @@ with DAG(
 ) as dag:
 
     insert_task = PythonOperator(
-        task_id="insert_records",
+        task_id="insert_new_records",
         python_callable=insert_records
     )
